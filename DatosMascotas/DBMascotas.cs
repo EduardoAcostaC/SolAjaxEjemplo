eb93b4be380dfef4f1a00fef0ea04b7e2cb42be7
EduardoAcostaC <eacostac1700@alumno.ipn.mx>
2023-10-25 13:07:51 -0600

    Agregar archivos de proyecto.

---
+++ DatosMascotas/DBMascotas.cs
@@ -0,0 +1,58 @@
+﻿using DatosMascotas.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosMascotas
+{
+    public class DBMascotas
+    {
+
+        private Generacion22Entities db = new Generacion22Entities();
+
+        public List<Mascotas> Obtener()
+        {
+            List<Mascotas> lista = db.Mascotas.ToList();
+            db.Dispose();
+            return lista;
+        }
+
+        public Mascotas Obtener(int id)
+        {
+            Mascotas mascota = db.Mascotas.Find(id);
+            db.Dispose();
+            return mascota;
+        }
+
+        public void Agregar(Mascotas mascota)
+        {
+            db.Mascotas.Add(mascota);
+            db.SaveChanges();
+            db.Dispose();
+        }
+        
+        public void Borrar(int id)
+        {
+            Mascotas mascota = db.Mascotas.Find(id);
+            db.Mascotas.Remove(mascota);
+            db.SaveChanges();
+            db.Dispose();
+        }
+
+        public void Editar(Mascotas mascota)
+        {
+            db.Mascotas.AddOrUpdate(mascota);
+            db.SaveChanges();
+            db.Dispose();
+        }
+        public List<Mascotas> Buscar(string valor)
+        {
+            List<Mascotas> lista = db.Mascotas.Where(x => x.nombre.Contains(valor)).ToList();
+            db.Dispose();
+            return lista;
+        }
+    }
+}
---
+++ WebAjaxEjemplo/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using DatosMascotas;
+using DatosMascotas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAjaxEjemplo.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBMascotas m = new DBMascotas();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Obtener()
+        {
+            try
+            {
+                List<Mascotas> lista = m.Obtener();
+                return Json( new {mensaje = "ok", ls = lista}, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new {mensaje = ex.Message}, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult ObtenerPorId(int id)
+        {
+            try
+            {
+                Mascotas mascota  = m.Obtener(id);
+                return Json(new { mensaje = "ok", mascota = mascota}, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult Agregar(Mascotas mascota)
+        {
+            try
+            {
+                m.Agregar(mascota);
+                return Json( new {mensaje = "ok"}, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult BorrarDeServidor(int id)
+        {
+            try
+            {
+                m.Borrar(id);
+                return Json(new { mensaje = "ok" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult Editar(Mascotas mascota)
+        {
+            try
+            {
+                m.Editar(mascota);
+                return Json(new { mensaje = "ok" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
